dd1537944edc11cac5e9f26ecb4fc80f235cbb41
Maizels Kirill <domo.ddr@gmail.com>
2018-06-04 11:53:56 +0600

    Some imporves

--- SocketMessengerServer/SocketMessengerServer/Program.cs
+++ SocketMessengerServer/SocketMessengerServer/Program.cs
@@ -5,26 +5,57 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
+using Newtonsoft.Json;
 
 namespace SocketMessengerServer
 {
     class Program
     {
-        private static int defaultPort = 3535;
+        private const int defaultPort = 3535;
+
         private static List<int> membersIds = new List<int>();
         private static int newMemberId = 1;
 
         static void Main(string[] args)
         {
-            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
-            socket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), defaultPort));
+            listenSocket.Bind(new IPEndPoint(IPAddress.Parse("10.3.6.62"), defaultPort));
 
-            while (true)
+            try
             {
-                socket.Listen(3);
+                listenSocket.Listen(3);
 
-                Console.WriteLine($"Пользователь №{newMemberId++} подключился");
+                while (true)
+                {
+                    Socket handler = listenSocket.Accept();
+
+                    int bytesRead = 0;
+                    byte[] allResponse = new byte[512];
+
+                    do
+                    {
+                        bytesRead = handler.Receive(allResponse);
+                    }
+                    while (handler.Available > 0);
+
+                    byte[] data = new byte[bytesRead];
+                    Array.Copy(allResponse, data, bytesRead);
+
+                    Message response = JsonConvert.DeserializeObject<Message>(Encoding.Default.GetString(data));
+
+                    Console.WriteLine($"{response.Sender} подключился к серверу");
+
+                    string message = JsonConvert.SerializeObject(response);
+                    handler.Send(Encoding.Default.GetBytes(message));
+
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
     }
