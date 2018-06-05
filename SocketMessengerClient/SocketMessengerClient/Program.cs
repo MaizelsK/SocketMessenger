7d96f12906b7b55161378f5fdcf9ccfe5c098d1d
Доспаев Таир Асылбекович <@corp.itstep>
2018-06-05 10:23:00 +0600

    Client2.0

--- SocketMessengerClient/SocketMessengerClient/Program.cs
+++ SocketMessengerClient/SocketMessengerClient/Program.cs
@@ -16,7 +16,7 @@ namespace SocketMessengerClient
         private static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         static void Main(string[] args)
         {
-           
+
 
             Client user = new Client();
             Console.WriteLine("Авторизация\n");
@@ -24,49 +24,70 @@ namespace SocketMessengerClient
             user.Sender = Console.ReadLine();
             SendMessage(user);
 
+            try
+            {
+                while (true)
+                {
+                    GetResponce();
+                }
+            }
+            catch (SocketException exception)
+            {
+                Console.WriteLine(exception.Message);
+                socket.Close();
+            }
 
-            Client receivedUser;
-            byte[] buffer = new byte[1024];
-            StringBuilder builder = new StringBuilder(); 
+
+
+
+
+
+        }
+
+
+        private static void GetResponce()
+        {
             int bytes = 0;
+            Client receivedUser;
+
+            byte[] buffer = new byte[1024];
             do
             {
                 bytes = socket.Receive(buffer, buffer.Length, 0);
-
-
-                builder.Append(Encoding.Default.GetString(buffer,0,bytes));
                 receivedUser = JsonConvert.DeserializeObject<Client>(Encoding.Default.GetString(buffer));
-
-
             }
             while (socket.Available > 0);
-           
-            Console.WriteLine("Пользователь:"+ receivedUser.Sender);
-            Console.WriteLine("Сообщение:" + receivedUser.MessageText);
 
+            Console.WriteLine(receivedUser.Sender + ":" + receivedUser.MessageText);
 
-            Console.ReadLine();
         }
 
         private static void SendMessage(Client client)
         {
-            socket.Connect("10.3.6.62", 3535);
-            Console.Clear();
-            Console.WriteLine("Авторизация успешно выполнено\n");
-            Console.WriteLine("Введите сообщение:");
-            client.MessageText= Console.ReadLine();
-            Console.Clear();
-
-                
-            string jsonObj= JsonConvert.SerializeObject(client);
-            byte[] buffer = Encoding.Default.GetBytes(jsonObj);
-
-           
-            socket.Send(buffer);
-           
+            try
+            {
+                socket.Connect("10.3.6.62", 3535);
+                Console.Clear();
+                Console.WriteLine("Авторизация успешно выполнено\n");
+                Console.WriteLine("Введите сообщение:");
+                client.MessageText = Console.ReadLine();
+                Console.Clear();
 
 
-            
+                string jsonObj = JsonConvert.SerializeObject(client);
+                byte[] buffer = Encoding.Default.GetBytes(jsonObj);
+
+
+                socket.Send(buffer);
+
+            }
+            catch (SocketException exception)
+            {
+                Console.WriteLine(exception.Message);
+                socket.Close();
+            }
+
+
         }
     }
 }
---
+++ SocketMessengerServer/SocketMessengerServer/SocketMessengerServer/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace SocketMessengerServer
+{
+    class Program
+    {
+        private const int defaultPort = 3535;
+        private static Socket listenSocket;
+
+        static void Main(string[] args)
+        {
+            listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            listenSocket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), defaultPort));
+
+            try
+            {
+                listenSocket.Listen(3);
+
+                while (true)
+                {
+                    Socket handler = listenSocket.Accept();
+
+                    Task.Run(() => HandleClient(handler));
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void HandleClient(Socket client)
+        {
+            ShowNewMember(client);
+
+            while (true)
+            {
+                try
+                {
+                    Message response = GetResponse(client);
+
+                    if (response != null)
+                    {
+                        string message = JsonConvert.SerializeObject(response);
+                        listenSocket.Send(Encoding.Default.GetBytes(message));
+                    }
+                    //client.Send(Encoding.Default.GetBytes(message));
+                }
+                catch (SocketException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    break;
+                }
+            }
+
+            client.Shutdown(SocketShutdown.Both);
+            client.Close();
+        }
+
+        private static void ShowNewMember(Socket client)
+        {
+            try
+            {
+                Message response = GetResponse(client);
+
+                if (response != null)
+                {
+                    string newMemberMessage = $"{response.Sender} подключился к серверу";
+                    listenSocket.SendTo(Encoding.Default.GetBytes(newMemberMessage), listenSocket.LocalEndPoint);
+
+                    string cleintMessage = JsonConvert.SerializeObject(response);
+                    listenSocket.SendTo(Encoding.Default.GetBytes(cleintMessage), listenSocket.LocalEndPoint);
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static Message GetResponse(Socket client)
+        {
+            Message response = null;
+
+            try
+            {
+                int bytesRead = 0;
+                byte[] allResponse = new byte[512];
+
+                do
+                {
+                    bytesRead = client.Receive(allResponse);
+                }
+                while (client.Available > 0);
+
+                byte[] data = new byte[bytesRead];
+                Array.Copy(allResponse, data, bytesRead);
+
+                response = JsonConvert.DeserializeObject<Message>(Encoding.Default.GetString(data));
+
+                return response;
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return response;
+        }
+    }
+}
