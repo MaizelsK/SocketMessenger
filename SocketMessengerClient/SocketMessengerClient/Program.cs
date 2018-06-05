da39d43bb0b47d2afb59c1796359b23dcc1cc7ca
Доспаев Таир Асылбекович <@corp.itstep>
2018-06-05 11:34:42 +0600

    Client 1.3

--- SocketMessengerClient/SocketMessengerClient/Program.cs
+++ SocketMessengerClient/SocketMessengerClient/Program.cs
@@ -16,19 +16,23 @@ namespace SocketMessengerClient
         private static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         static void Main(string[] args)
         {
-
-
             Client user = new Client();
-            Console.WriteLine("Авторизация\n");
-            Console.WriteLine("Введите имя:");
-            user.Sender = Console.ReadLine();
-            SendMessage(user);
+
+            Authorize(user);
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    GetResponce();
+                }
+            });
 
             try
             {
                 while (true)
                 {
-                    GetResponce();
+                    SendMessage(user.Sender);
                 }
             }
             catch (SocketException exception)
@@ -36,59 +40,70 @@ namespace SocketMessengerClient
                 Console.WriteLine(exception.Message);
                 socket.Close();
             }
-
-
-
-
-
-
         }
 
+        private static void Authorize(Client user)
+        {
+            Console.WriteLine("Авторизация\n");
+            Console.WriteLine("Введите имя:");
+
+            user.Sender = Console.ReadLine();
+            socket.Connect("10.3.6.62", 3535);
+
+            Console.Clear();
+            Console.WriteLine("Авторизация успешно выполнено\n");
+        }
 
         private static void GetResponce()
         {
-            int bytes = 0;
-            Client receivedUser;
-
-            byte[] buffer = new byte[1024];
-            do
-            {
-                bytes = socket.Receive(buffer, buffer.Length, 0);
-                receivedUser = JsonConvert.DeserializeObject<Client>(Encoding.Default.GetString(buffer));
-            }
-            while (socket.Available > 0);
-
-            Console.WriteLine(receivedUser.Sender + ":" + receivedUser.MessageText);
-
-        }
-
-        private static void SendMessage(Client client)
-        {
-             socket.Connect("10.3.6.62", 3535);
-
             try
             {
-                socket.Connect("10.3.6.62", 3535);
-                Console.Clear();
-                Console.WriteLine("Авторизация успешно выполнено\n");
-                Console.WriteLine("Введите сообщение:");
-                client.MessageText = Console.ReadLine();
-                Console.Clear();
+                int bytes = 0;
+                Client receivedUser;
 
+                byte[] buffer = new byte[1024];
+                do
+                {
+                    bytes = socket.Receive(buffer, buffer.Length, 0);
 
-                string jsonObj = JsonConvert.SerializeObject(client);
+                    byte[] data = new byte[bytes];
+                    Array.Copy(buffer, data, bytes);
+
+                    receivedUser = JsonConvert.DeserializeObject<Client>(Encoding.Default.GetString(data));
+                }
+                while (socket.Available > 0);
+
+                Console.WriteLine(receivedUser.Sender + ":" + receivedUser.MessageText + "\n");
+            }
+            catch(SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void SendMessage(string username)
+        {
+            try
+            {
+                Client newMessage = new Client
+                {
+                    Sender = username
+                };
+
+                //Console.Write("Введите сообщение: ");
+                newMessage.MessageText = Console.ReadLine();
+                Console.WriteLine();
+
+                string jsonObj = JsonConvert.SerializeObject(newMessage);
                 byte[] buffer = Encoding.Default.GetBytes(jsonObj);
                 
                 socket.Send(buffer);
-
             }
             catch (SocketException exception)
             {
                 Console.WriteLine(exception.Message);
                 socket.Close();
             }
-
-
         }
     }
 }
